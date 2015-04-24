eaeba56a827cc9042e28afa4e77e6b9481f60c29
jhare9 <jonathon.hare@eagles.usm.edu>
2015-04-24 01:29:39 -0500

    modified game class with a better game loop and timer for the foods

--- src/snake_with_a_twist/Game.java
+++ src/snake_with_a_twist/Game.java
@@ -7,7 +7,6 @@ package snake_with_a_twist;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
@@ -18,7 +17,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
@@ -30,239 +28,246 @@ import javax.swing.JPanel;
  *
  * @author jon
  */
-public class Game extends JPanel implements Runnable,KeyListener{
+public class Game extends JPanel implements Runnable, KeyListener{
+    // screen sizes and block sizes
+    private final static int WIDTH = 500; 
+    private final static int HEIGHT = 500;
+    private final static int BOX_WIDTH = 25;
+    private final static int BOX_HEIGHT = 25;
     
-    private final static int WIDTH = 400;
-    private final static int HEIGHT = 400;
-    private boolean running = false;
-    private Thread gameThread; 
-    private LinkedList<Point> snake; 
-    int snake_size; 
-    private int snakeX; 
-    private int snakeY; 
-    private int direction;
-    private boolean right; 
-    private boolean left; 
-    private boolean down; 
-    private boolean up;
+    public static boolean running; // boolean to tell if the game is running or not
+    private Thread gameThread; // game thread
+    // the snakes parts 
+    private LinkedList<Point> snake;
+    private Point snakeParts;  
+    private int snakeLength;
+    // the snakes food
+    private Point food; 
     private boolean normalFood;
     private boolean poision;
-    private double totalElaspedTime;
-    private Point food; 
-    private int highScore;
-    private int score;
+    // the directions
+    private boolean right;
+    private boolean left; 
+    private boolean up; 
+    private boolean down; 
+    // score variables
+    private int score; 
+    private int highScore; 
     
+    //boolean to detect colisions
+    private boolean colision;
+    
+    //timer for the random generation of foods 
+    private long timer;
+    private double timeInSec;
+    // random for food
+    private Random n;
+    
+    //the game constructor
     public Game(){
         setPreferredSize(new Dimension(WIDTH,HEIGHT));
         setFocusable(true);
-        this.addKeyListener(this);
-        new_game();  
- }
+        addKeyListener(this); 
+        
+        start_game();
+    }
     
-    public void new_game(){
-        running = true;
-        gameThread = new Thread(this,"game thread");
+    private void init(){
         
-        snake = new LinkedList<Point>();
+        snakeParts.setLocation(5,10);
+        snake.addFirst(snakeParts);
+        snakeLength = 3; 
         
-        snake_size = 3;
+        normalFood = true; 
+        poision = false; 
+        food.setLocation(5,5);
         
-        normalFood = true;
-        poision = false;
+        down = false;
+        up = false; 
+        right = false; 
+        left = false; 
+        score = 0;    
         
-        food = new Point();
-        food.setLocation(10,10);
+        // for right now 
+        colision = false; 
         
-        direction = 4;
-        down = true;
-        up = false;
-        right = false;
-        left = false;
-        score = 0;
-        snakeX = 5; 
-        snakeY = 5;
         fileReader("highscore.txt");
-        System.out.println("high score "+highScore);
-        gameThread.start();
-      }
+    }
+    
+    
+    public synchronized void start_game(){
+            snake = new LinkedList<>();     
+            snakeParts = new Point();
+            food = new Point();
+            n = new Random();
+            init();
+            running = true;
+            gameThread = new Thread(this,"game thread");
+            gameThread.start();
+    }
     
     public void update(){
-             
-            //add a new snake part   
-            snake.addFirst(new Point(snakeX,snakeY));
-            
-            // keeps the snake the same length
-            if(snake.size() > snake_size)
-                snake.removeLast();
-            
-             // deals with the arrows keys for the snake directions 
-            // and the movement of the snake
-            switch(direction){
-                case 1:
-                     snakeY--;
-                    break;
-                case 2: 
-                    snakeX++;
-                    break;
-                case 3: 
-                    snakeX--;
-                    break;
-                case 4:
-                    snakeY++;
-                    break;
-            }
-            
-            
-            // checks for to see if the snake colide with its self
-            for(int i = 1; i<snake.size(); ++i){
-                if(snake.getFirst().x == snake.get(i).x && snake.getFirst().y == snake.get(i).y){
-                    running = false;
-                    fileWriter("highscore.txt");
-                }
-            }
-            
-            // food colison
-            if(snake.getFirst().x == food.x && snake.getFirst().y == food.y){
-                if(normalFood){   
-                    snake_size++;
-                    score++;
-                    
-                }else if(poision){
-                    snake_size--;
-                    score--;
-                    snake.removeLast();
-                    if(snake_size <=1){
-                        running = false;
-                        fileWriter("highscore.txt");
-                    }
-                }
-                
-                generateFood();
-                
-                Random n = new Random();
-                food.setLocation(n.nextInt(WIDTH /20),n.nextInt(HEIGHT / 20));
-            }
-            
-            if(totalElaspedTime >= 3.0){
-                generateFood();
-                totalElaspedTime = 0.0;
-            }
-      
-            
-            //dealing with wall colisions
-            if( snake.getFirst().getY()*20 >= HEIGHT-19.5 || snake.getFirst().getY() *20 <= -1 ||
-                    snake.getFirst().getX() * 20 >= WIDTH -19.5 || snake.getFirst().getX() * 20 <= -1){
-                
-                fileWriter("highscore.txt");
-                running = false;
-            }           
-            
-    }   
-        
-
-    @Override
-    public void paint(Graphics g) {
-       // clear the screen to repaint 
-       g.clearRect(0, 0,WIDTH,HEIGHT);
-       
-        // paint the food to the screen
-        if(normalFood){
-            g.setColor(Color.red);
-            
-        }
-        else if(poision){
-            g.setColor(Color.green);
-        }
-        
-        g.fillRect(food.x * 20,food.y*20,20,20);
-       
-       //paint the snake to the screen
-       for(int i = 0; i < snake.size(); i++){
-           g.setColor(Color.BLUE);
-           g.fillRect((int)(snake.get(i).x*20),(int)(snake.get(i).y*20), 20, 20);
-       }
-       
-       g.setColor(Color.BLACK);
-        // paint the verticle lines 
-       for(int x = 0; x < WIDTH; x++)
-           g.drawLine(x*20,0,x*20,HEIGHT);
-       
-       //paint the horrizontal lines
-       for(int y = 0; y <HEIGHT; y++)
-            g.drawLine(0,y * 20,WIDTH, y*20);
-       
-       //paint scoring system
-       g.setColor(Color.RED);
-       g.drawString("high score: "+highScore,20,20);
-       g.drawString("your Score: "+score,20,HEIGHT -20);
-      
-    }
-
-    @Override
-    public void run(){
-        // game loop runs forever unless the player hits a wall or another snake part
-        long startTime = System.currentTimeMillis();
-        
-        while(running){
-            
-            long currentTime = System.currentTimeMillis();
-            double elaspedTime = currentTime - startTime;
-            totalElaspedTime += elaspedTime / 1000;
-            
-            update();
-            repaint();
-            
-            try {
-                Thread.sleep(150);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            
-            startTime = currentTime;
-        }
-        
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-    }
-   
-    public void generateFood(){
-         Random n = new Random();
-                
+          
+          
+          long elaspedTime = System.currentTimeMillis();
+          timeInSec += (elaspedTime - timer) / 1000.0; 
+          
+          if(timeInSec > 5.0){
+                timer = elaspedTime;
+                timeInSec = 0.0;
                 switch(n.nextInt(2)){
                     case 0:
-                           normalFood = true;
-                           poision = false;
+                        normalFood = true;
+                        poision = false;
                         break;
                     case 1:
-                        poision = true;
                         normalFood = false;
+                        poision = true;
                         break;
+                }  
+            }
+        
+          if(!colision){
+                   
+            //add a new point to the head of the snake
+            snake.addFirst(new Point(snakeParts.x,snakeParts.y)); 
+            
+             // delete the last elements if its longer than the length
+            if(snake.size() > snakeLength)
+                snake.removeLast();
+           
+             // colision dections 
+            for(int s = 3; s < snake.size(); s++)
+                if(snake.getFirst().x == snake.get(s).x && snake.getFirst().y == snake.get(s).y){
+                            colision = true;
+                            fileWriter("highscore.txt");
                 }
+            
+            if(snake.getFirst().y * BOX_HEIGHT > HEIGHT - BOX_HEIGHT || snake.getFirst().y * BOX_HEIGHT < 0
+                    || snake.getFirst().x * BOX_WIDTH > WIDTH - BOX_WIDTH || snake.getFirst().x < 0){
+                colision = true; 
+                fileWriter("highscore.txt");
+            }
+            
+            if(snake.getFirst().y == food.y && snake.getFirst().x == food.x){
+                if(normalFood){
+                   snakeLength++;
+                   score++;
+                }
+                else if(poision){
+                   snakeLength--;
+                   if(snakeLength < 2)
+                       colision = true;
+                   snake.removeLast();
+                   if(score != 0)
+                       score--;
+                }
+                food.setLocation(new Random().nextInt(WIDTH / BOX_WIDTH),new Random().nextInt(HEIGHT / BOX_HEIGHT));
+            }
+            
+            
+            
+            if(down)
+                snakeParts.y++;
+            else if(up)
+                snakeParts.y--;
+            else if(right)
+                snakeParts.x++;
+            else if (left)
+                snakeParts.x--;
+            
+          }
+          
+          
     }
     
-    public void fileReader(String fileName){
-        Scanner file_in = null;
+    @Override
+    public void paint(Graphics g){
+        
+        g.clearRect(0,0,WIDTH,HEIGHT);
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,WIDTH,HEIGHT);
+        
+        
+       
+        // draw the snake 
+        g.setColor(Color.GREEN);
+        for(int s = 0; s < snake.size(); s++)
+            g.fillRect(snake.get(s).x * BOX_WIDTH, snake.get(s).y * BOX_HEIGHT, BOX_WIDTH,BOX_HEIGHT);
+        
+        // draw food for the snake
+        if(normalFood)
+            g.setColor(Color.red);
+        else if(poision)
+            g.setColor(Color.yellow);
+        
+        g.fillRect(food.x * BOX_WIDTH,food.y * BOX_HEIGHT,BOX_WIDTH,BOX_HEIGHT);
+        
+         // draw the verticle lines
+        g.setColor(Color.gray);
+        
+        for(int x = 0; x < WIDTH; x++)
+            g.drawLine(x*BOX_WIDTH,0,x * BOX_WIDTH, HEIGHT);
+        
+        // draw the horizontal lines 
+        for(int y = 0; y < HEIGHT; y++)
+            g.drawLine(0,y * BOX_HEIGHT,WIDTH, y * BOX_HEIGHT);
+        
+       //paint scoring system
+       g.setColor(Color.white);
+       g.drawString("high score: "+highScore,20,20);
+       g.drawString("your Score: "+score,20,HEIGHT -20);
+        
+    }
+    
+    public synchronized void stop_game(){
+       
         try {
-            
-            file_in = new Scanner(new File(fileName));
-            
-            while(file_in.hasNextLine()){
-                highScore = file_in.nextInt();
-            }
-            
-        } catch (Exception e) {
-           System.out.println("cant find file");
-        }finally{
-            try{
-            file_in.close();
-            }catch(Exception e){
-                System.out.println("null pointer");
-            }
+            gameThread.join();
+        } catch (InterruptedException ex) {
+            System.out.println("having trouble closing the thread");
         }
+    }
+    
+    @Override
+    public void run() {
+        // start the timer a 5 updates per second
+        long startTime = System.nanoTime();
+        double optTime = (1000000000.0 / 5.0);
+        double delta = 0;
+        int updates = 0;
+        int frames = 0; 
+        long timer = System.currentTimeMillis();
+        //other timer 
+        timer = System.currentTimeMillis();
+        // the game loop 
+        while(running){
+            long elaspedTime = System.nanoTime();
+            delta += (elaspedTime - startTime) / optTime;
+            startTime = elaspedTime; 
+            
+            
+             while(delta >= 1.0 ){
+                 update();
+                 updates++;
+                delta--;
+            }
+             
+              repaint();
+              frames++;
+             
+             if(System.currentTimeMillis() - timer > 1000){
+                 timer += 1000;
+                 System.out.println("updates "+updates + " frames per sec "+frames);
+                 updates = 0;
+                 frames = 0;
+            } 
+             
+            
+        }
+        
+        stop_game();
+           
     }
     
     public void fileWriter(String fileName){
@@ -289,54 +294,72 @@ public class Game extends JPanel implements Runnable,KeyListener{
         
     }
     
-    // not used 
+     public void fileReader(String fileName){
+        Scanner file_in = null;
+        try {
+            
+            file_in = new Scanner(new File(fileName));
+            
+            while(file_in.hasNext()){
+                highScore = file_in.nextInt();
+            }
+            
+        } catch (FileNotFoundException ex) {
+           System.out.println("cant find file");
+        }finally{
+            try{
+            file_in.close();
+            }catch(Exception e){
+                System.out.println("null pointer");
+            }
+        }
+    }
+    
     @Override
     public void keyTyped(KeyEvent e) {}
 
-    // handles the arrow keys 
     @Override
     public void keyPressed(KeyEvent e) {
-       switch(e.getKeyCode()){
-           case KeyEvent.VK_UP:
-               if(!down){
-               direction = 1;
-               }
-               up = true; 
-               right = false; 
-               left = false;
-               break; 
-           case KeyEvent.VK_RIGHT:
-               if(!left){
-               direction = 2;
-               }
-               right = true;
-               up = false;
-               down = false; 
-               break;
-           case KeyEvent.VK_LEFT:
-               if(!right){
-               direction = 3;
-               }
-               up = false; 
-               down = false;
-               left = true;
-               break;
-           case KeyEvent.VK_DOWN:
-               if(!up){
-               direction = 4;
-               }
-               down = true; 
-               left = false; 
-               right = false; 
-               break;
-       }
-       
-       if( e.getKeyCode() == KeyEvent.VK_SPACE)
-            new_game();
-           
+        
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_UP:
+                if(!down)
+                    up = true;
+                right = false; 
+                left = false; 
+                break;
+            case KeyEvent.VK_DOWN:
+                if(!up)
+                    down = true;
+                right = false; 
+                left = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                if(!left)
+                    right = true;
+                up = false;
+                down = false; 
+                break;
+            case KeyEvent.VK_LEFT:
+                if(!right)
+                    left = true;
+                up = false; 
+                down = false; 
+                break; 
+            case KeyEvent.VK_SPACE:
+                snake.clear();
+                init();
+                break; 
+            case KeyEvent.VK_ESCAPE:
+                running = false;
+                System.exit(0);
+                break;
+            }
+            
     }
-    // not used 
+
     @Override
     public void keyReleased(KeyEvent e) {}
-
-    }
+    
+    
+}
